91097bc0cef1c8ec57667a6777eecc5a4e7a3782
mixypro <mixypro@yahoo.com>
2019-03-06 13:31:09 +0100

    Final almost

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // !! IMPORTANT README:
 
 // You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+// When the user scrolls the page, execute myFunction 
 
 /***********
 INSTRUCTIONS:
